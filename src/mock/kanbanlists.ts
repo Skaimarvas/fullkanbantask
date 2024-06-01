4d0813515af8b8c68e1423e2e6ada449651c85ea
Kaim Arvas <skaimarvas@gmail.com>
2024-06-01 14:54:38 +0300

    mockData was created

---
+++ src/mock/kanbanlists.ts
@@ -0,0 +1,81 @@
+export const kanbanlist = {
+  backLog: [
+    {
+      title: "Twilio integration",
+      description:
+        "Create new note via SMS. Support text, audio, links, and media.",
+      status: "",
+    },
+    {
+      title: "Markdown support",
+      description: "Markdown shorthand converts to formatting",
+      status: "Formatting",
+    },
+  ],
+  toDo: [
+    {
+      title: "Tablet view",
+      description: "",
+      status: "",
+    },
+    {
+      title: "Mobile view",
+      description:
+        "Functions for both web responsive and native apps. Note: Android and iOS will need unique share icons.",
+      status: "",
+    },
+    {
+      title: "Audio recording in note",
+      description: "Show audio in a note and playback UI",
+      status: "Note interface",
+    },
+    {
+      title: "Bookmark in note",
+      description:
+        "Show rich link UI in a note, and feature to render website screenshot.",
+      status: "Note interface",
+    },
+    {
+      title: "Image viewer",
+      description:
+        "Opens when clicking on the thumbnail in the list or on the image in the note",
+      status: "",
+    },
+  ],
+  inProgress: [
+    {
+      title: "Desktop view",
+      description:
+        "PWA for website and native apps. Note: Windows and Mac will need unique share icons.",
+      status: "",
+    },
+    {
+      title: "Mobile home screen",
+      description: "Folders, tags, and notes lists are sorted by recent.",
+      status: "",
+    },
+    {
+      title: "Formatting options",
+      description:
+        "Mobile formatting bar expands and collapses when tapping the format icon.",
+      status: "",
+    },
+    {
+      title: "Media in note",
+      description: "Image & video with player UI",
+      status: "Note interface",
+    },
+  ],
+  designed: [
+    {
+      title: "Audio recording",
+      description: "Interface for when recording a new audio note",
+      status: "New Note",
+    },
+    {
+      title: "Bookmarking",
+      description: "Interface for when creating a new link note.",
+      status: "New Note",
+    },
+  ],
+};
