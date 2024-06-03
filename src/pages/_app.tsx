c83c4e388cb37c3a85585f15909007a4d8f49610
Kaim Arvas <skaimarvas@gmail.com>
2024-06-03 10:37:07 +0300

    add apiprovider and queryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
+import ApiProvider from "@/api/ApiProvider";
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: false,
+        staleTime: twentyFourHoursInMs,
+      },
+    },
+  });
+  return (
+    <>
+      <ApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ApiProvider>
+    </>
+  );
 }
