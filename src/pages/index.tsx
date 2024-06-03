ff649753e98bcda9322dbf878f38de680f698b20
Kaim Arvas <skaimarvas@gmail.com>
2024-06-03 10:38:15 +0300

    add getListQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
+import { getListQuery } from "@/api/controller/ListController";
 import Column from "@/components/Column";
-import {
-  INITIAL_COLUMN_ORDER,
-  INITIAL_COL_DATA,
-  ITEMS,
-} from "@/mock/kanbanlists";
 import {
   DragDropContext,
   Draggable,
   Droppable,
   resetServerContext,
 } from "@hello-pangea/dnd";
-import axios from "axios";
-import { error } from "console";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 
@@ -25,8 +19,11 @@ export async function getServerSideProps(context: any) {
 }
 
 export default function Home() {
-  const [columnsOrder, setColumnsOrder] = useState(INITIAL_COLUMN_ORDER);
-  const [data, setData] = useState<any>(INITIAL_COL_DATA);
+  const { data: kanbanList, isLoading, isFetched } = getListQuery();
+
+  const [columnsOrder, setColumnsOrder] = useState<any>();
+  const [data, setData] = useState<any>();
+  const [items, setItems] = useState<any>();
 
   const handleDragDrop = (results: any) => {
     const { source, destination, type } = results;
@@ -82,93 +79,21 @@ export default function Home() {
     }
   };
 
+  console.log("Data", kanbanList);
+
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/list")
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  }, []);
+    if (isFetched) {
+      setColumnsOrder(kanbanList?.order);
+      setData(kanbanList?.column);
+      setItems(kanbanList?.items);
+    }
+  }, [data, isFetched]);
 
   return (
     <main
       className={`flex min-h-screen w-fit flex-row  justify-start items-start gap-4 p-4 ${inter.className}`}
     >
-      {/* <div className="flex flex-col bg-[#262626] h-fit p-6 gap-4 w-[400px] rounded-2xl">
-        <h3 className="text-2xl tracking-wide font-bold">Backlog</h3>
-        {kanbanlist &&
-          kanbanlist.backLog.map((back) => (
-            <button className="flex flex-col bg-[#6A6DCD] hover:bg-[#575aa8]  rounded-lg p-6 gap-2">
-              <h4 className=" text-base font-bold "> {back.title} </h4>
-              <p className="text-sm text-left">{back.description}</p>
-              {back.status && (
-                <div className="px-1 border border-white w-fit ">
-                  <span className="text-sm"> {back.status} </span>
-                </div>
-              )}
-            </button>
-          ))}
-        <button className="p-6 w-full border border-white">
-          <span>+ Create</span>
-        </button>
-      </div>
-      <div className="flex flex-col bg-[#262626] h-fit p-6 gap-4 w-[400px] rounded-2xl">
-        <h3 className="text-2xl tracking-wide font-bold">To do</h3>
-        {kanbanlist &&
-          kanbanlist.toDo.map((to) => (
-            <button className="flex flex-col bg-[#6A6DCD] hover:bg-[#575aa8] rounded-lg p-6 gap-2">
-              <h4 className=" text-base font-bold "> {to.title} </h4>
-              <p className="text-sm text-left">{to.description}</p>
-              {to.status && (
-                <div className="px-1 border border-white w-fit ">
-                  <span className="text-sm"> {to.status} </span>
-                </div>
-              )}
-            </button>
-          ))}
-        <button className="p-6 w-full border border-white">
-          <span>+ Create</span>
-        </button>
-      </div>
-      <div className="flex flex-col bg-[#262626] h-fit p-6 gap-4 w-[400px] rounded-2xl">
-        <h3 className="text-2xl tracking-wide font-bold">In progress</h3>
-        {kanbanlist &&
-          kanbanlist.inProgress.map((prog) => (
-            <button className="flex flex-col bg-[#6A6DCD] hover:bg-[#575aa8] rounded-lg p-6 gap-2">
-              <h4 className=" text-base font-bold "> {prog.title} </h4>
-              <p className="text-sm text-left">{prog.description}</p>
-              {prog.status && (
-                <div className="px-1 border border-white w-fit ">
-                  <span className="text-sm"> {prog.status} </span>
-                </div>
-              )}
-            </button>
-          ))}
-        <button className="p-6 w-full border border-white">
-          <span>+ Create</span>
-        </button>
-      </div>
-      <div className="flex flex-col bg-[#262626] h-fit p-6 gap-4 w-[400px] rounded-2xl">
-        <h3 className="text-2xl tracking-wide font-bold">Designed</h3>
-        {kanbanlist &&
-          kanbanlist.designed.map((design) => (
-            <button className="flex flex-col bg-[#6A6DCD] hover:bg-[#575aa8] rounded-lg p-6 gap-2">
-              <h4 className=" text-base font-bold "> {design.title} </h4>
-              <p className="text-sm text-left">{design.description}</p>
-              {design.status && (
-                <div className="px-1 border border-white w-fit ">
-                  <span className="text-sm"> {design.status} </span>
-                </div>
-              )}
-            </button>
-          ))}
-        <button className="p-6 w-full border border-white">
-          <span>+ Create</span>
-        </button>
-      </div>
-
-      <button className="p-6 border border-white">
-        <span className="text-3xl">+</span>
-      </button> */}
+      {" "}
       <DragDropContext onDragEnd={handleDragDrop}>
         <Droppable droppableId="ROOT" type="COLUMN" direction="horizontal">
           {(provided) => (
@@ -177,7 +102,7 @@ export default function Home() {
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
-              {columnsOrder.map((colId, index) => {
+              {columnsOrder?.map((colId: any, index: number) => {
                 const columnData = data[colId];
                 return (
                   <Draggable
@@ -202,7 +127,7 @@ export default function Home() {
                         </div>
 
                         {/* Render items within the column */}
-                        <Column {...columnData} ITEMS={ITEMS} />
+                        <Column {...columnData} ITEMS={items} />
                         <button className="p-6 w-full border border-white">
                           <span>+ Create</span>
                         </button>
