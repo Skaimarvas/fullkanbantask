17486fef0dd23e32f168aea80d2fbfd1ed576212
Kaim Arvas <skaimarvas@gmail.com>
2024-06-08 16:11:27 +0300

    create BoardSelector component

---
+++ src/components/BoardSelector.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+interface Iprops {
+  boards: any;
+  selectedBoard: any;
+  handleSelect: any;
+}
+
+const BoardSelector: React.FC<Iprops> = ({
+  boards,
+  selectedBoard,
+  handleSelect,
+}) => (
+  <Select value={selectedBoard} onValueChange={handleSelect}>
+    <SelectTrigger className="w-[180px]">
+      <SelectValue placeholder="Select a board" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel>Board</SelectLabel>
+        {boards?.map((board: any) => (
+          <SelectItem key={board.id} value={String(board.id)}>
+            {board.title}
+          </SelectItem>
+        ))}
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+);
+
+export default BoardSelector;
