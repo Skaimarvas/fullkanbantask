9d6d7dd00855f625634f7749b86894cd3496d02b
Kaim Arvas <skaimarvas@gmail.com>
2024-06-08 16:10:28 +0300

    remove express.js folder , add on root project

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "lumosmaxima",
+    database: "postgres",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres",
+    schema: "kanban",
+  },
+};
