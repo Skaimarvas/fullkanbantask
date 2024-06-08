a24ec64a02e2035aa2b53dc96acd1ad3e276775b
Kaim Arvas <skaimarvas@gmail.com>
2024-06-08 16:16:53 +0300

    change homepage for creating new boards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,145 +1,115 @@
-import { getListQuery } from "@/api/controller/ListController";
-import Column from "@/components/Column";
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  resetServerContext,
-} from "@hello-pangea/dnd";
-import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
+"use client";
 
+import {
+  createKanbanBoardRequest,
+  getKanbanBoardListQuery,
+} from "@/api/controller/BoardController";
+import { Button } from "@/components/ui/button";
+
+import BoardForm from "@/components/BoardForm";
+import BoardSelector from "@/components/BoardSelector";
+import { Separator } from "@/components/ui/separator";
+import { boardListGetType } from "@/interfaces/Board";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { z } from "zod";
 const inter = Inter({ subsets: ["latin"] });
 
-export async function getServerSideProps(context: any) {
-  resetServerContext();
-  return {
-    props: {},
-  };
-}
+const defaultBoard = {
+  title: "",
+};
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(2, {
+      message: "The board name must contain at least 2 characters.",
+    })
+    .max(20, {
+      message: "The board name must not exceed 20 characters.",
+    }),
+});
+
+export type BoardFormSchemaType = z.infer<typeof formSchema>;
 
 export default function Home() {
-  const { data: kanbanList, isLoading, isFetched } = getListQuery();
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultBoard,
+  });
+  const [selectedBoard, setSelectedBoard] = useState();
 
-  const [columnsOrder, setColumnsOrder] = useState<any>();
-  const [data, setData] = useState<any>();
-  const [items, setItems] = useState<any>();
+  const { data, refetch, isFetched } = getKanbanBoardListQuery();
+  const [boards, setBoards] = useState<boardListGetType[]>();
 
-  const handleDragDrop = (results: any) => {
-    const { source, destination, type } = results;
+  const [isBoard, setIsBoard] = useState<boolean>(false);
 
-    if (!destination) return;
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
+  const createBoard = useMutation({
+    mutationFn: createKanbanBoardRequest,
+    onSuccess: () => {
+      toast.success("Board was created successfully!");
+      setIsBoard(false);
+      form.reset();
+      refetch();
+    },
+    onError: (error) => {
+      console.error("Failed to create board:", error);
+    },
+  });
 
-    const sourceIndex = source.index;
-    const destinationIndex = destination.index;
+  const onBoardSubmit = (values: z.infer<typeof formSchema>) => {
+    createBoard.mutate(values);
+  };
 
-    if (type === "COLUMN") {
-      const reorderedColumns = [...columnsOrder];
-      const [removedItem] = reorderedColumns.splice(sourceIndex, 1);
-      reorderedColumns.splice(destinationIndex, 0, removedItem);
-
-      setColumnsOrder(reorderedColumns);
-
-      return;
-    } else {
-      if (source.droppableId === destination.droppableId) {
-        const source_col_id = source.droppableId;
-        const new_items_id_collection = [...data[source_col_id].itemsOrder];
-        const [deleted_item_id] = new_items_id_collection.splice(
-          sourceIndex,
-          1
-        );
-        new_items_id_collection.splice(destinationIndex, 0, deleted_item_id);
-        const new_data: any = { ...data };
-        new_data[source_col_id].itemsOrder = new_items_id_collection;
-        setData(new_data);
-      } else {
-        const source_col_id = source.droppableId,
-          dest_col_id = destination.droppableId;
-
-        const new_source_items_id_collc = [...data[source_col_id].itemsOrder];
-        const new_dest_items_id_collc = [...data[dest_col_id].itemsOrder];
-        const [deleted_item_id] = new_source_items_id_collc.splice(
-          sourceIndex,
-          1
-        );
-
-        new_dest_items_id_collc.splice(destinationIndex, 0, deleted_item_id);
-        const new_data = { ...data };
-        new_data[source_col_id].itemsOrder = new_source_items_id_collc;
-        new_data[dest_col_id].itemsOrder = new_dest_items_id_collc;
-
-        setData(new_data);
-      }
+  const handleSelect = (value: string) => {
+    setSelectedBoard(value as any);
+    if (value) {
+      router.replace(`/kanban/${value}`);
     }
   };
 
-  console.log("Data", kanbanList);
+  const handleIsBoard = () => {
+    setIsBoard(false);
+    form.reset();
+  };
 
   useEffect(() => {
     if (isFetched) {
-      setColumnsOrder(kanbanList?.order);
-      setData(kanbanList?.column);
-      setItems(kanbanList?.items);
+      setBoards(data);
     }
   }, [data, isFetched]);
 
   return (
     <main
-      className={`flex min-h-screen w-fit flex-row  justify-start items-start gap-4 p-4 ${inter.className}`}
+      className={`flex min-h-screen w-full flex-col justify-center items-center gap-4 p-4 ${inter.className}`}
     >
-      {" "}
-      <DragDropContext onDragEnd={handleDragDrop}>
-        <Droppable droppableId="ROOT" type="COLUMN" direction="horizontal">
-          {(provided) => (
-            <div
-              className="flex min-h-screen w-fit flex-row  justify-start items-start gap-4 p-4"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {columnsOrder?.map((colId: any, index: number) => {
-                const columnData = data[colId];
-                return (
-                  <Draggable
-                    draggableId={columnData.id}
-                    key={columnData.id}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <div
-                        className="flex flex-col bg-[#262626] h-fit p-6 gap-4 w-[400px] rounded-2xl"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                      >
-                        <div
-                          {...provided.dragHandleProps}
-                          className="flex items-center justify-between w-fit "
-                        >
-                          <h3 className="text-2xl tracking-wide font-bold">
-                            {" "}
-                            {columnData.title}{" "}
-                          </h3>
-                        </div>
-
-                        {/* Render items within the column */}
-                        <Column {...columnData} ITEMS={items} />
-                        <button className="p-6 w-full border border-white">
-                          <span>+ Create</span>
-                        </button>
-                      </div>
-                    )}
-                  </Draggable>
-                );
-              })}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <BoardSelector
+        boards={boards}
+        selectedBoard={selectedBoard}
+        handleSelect={handleSelect}
+      />
+      <Separator className="my-4 w-[180px]" />
+      {!isBoard ? (
+        <Button
+          className="w-[180px]"
+          variant="secondary"
+          onClick={() => setIsBoard(true)}
+        >
+          Create New Board
+        </Button>
+      ) : (
+        <BoardForm
+          form={form}
+          onSubmit={onBoardSubmit}
+          onCancel={handleIsBoard}
+        />
+      )}
     </main>
   );
 }
