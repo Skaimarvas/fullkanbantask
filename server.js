1143bd0b096a800525d683b54d9a740e22ab0812
Kaim Arvas <skaimarvas@gmail.com>
2024-06-18 17:04:31 +0300

    remove unnecessary import

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { Sequelize, DataTypes } = require("sequelize");
-const { title } = require("process");
 const app = express();
 
 app.use(express.json());
